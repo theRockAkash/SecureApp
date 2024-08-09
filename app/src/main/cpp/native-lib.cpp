892a697acae611c14ec095c82a58d62695adbbd7
theRockAkash <therockakash77@gmail.com>
2024-08-09 15:21:06 +0530

    key changed

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,14 +5,14 @@
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_secure_app_util_KeyUtils_secretKey(JNIEnv *env, jobject thiz) {
-    std::string secret_key = "ThisIsAPrivateKey 007";
+    std::string secret_key = "This is a highly secure data *** 007";
     return env->NewStringUTF(secret_key.c_str());
 }
 
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_secure_app_util_KeyUtils_aesKey(JNIEnv *env, jobject thiz) {
-    std::string aes_key = "at+EnFJmQIG5pHDRu8URdCrmKNaoCxx6qkx2+JuSZy0=";
+    std::string aes_key = "bRjT/nXfQNk+A7b91Du0jzCwIXHYa7BWfgG7l4eh6Tc=";
 
     return env->NewStringUTF(aes_key.c_str());
 }
